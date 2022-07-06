6e0d6e33d8920e64d9b3d9ad168ddf675989fdc7
Zaid_ZiD <zid6177@gmail.com>
2022-07-07 01:28:38 +0500

    update app.js

--- app.js
+++ app.js
@@ -3,20 +3,61 @@ let data = ()=>{
     let city = document.getElementById('city').value;
     let country = document.getElementById('country').value;
     let form = document.getElementById('form');
+    let Fajar = document.getElementById('Fajar')
+    let Zohar = document.getElementById('Zohar')
+    let Asar = document.getElementById('Asar')
+    let Maghrib = document.getElementById('Maghrib')
+    let Esha = document.getElementById('Esha')
+    let Fajarbox = document.getElementById('Fajarbox')
+    let Zoharbox = document.getElementById('Zoharbox')
+    let Asarbox = document.getElementById('Asarbox')
+    let Maghribbox = document.getElementById('Maghribbox')
+    let Eshabox = document.getElementById('Eshabox')
     
     axios.get(`https://api.aladhan.com/v1/timingsByCity?city=${city}&country=${country}&method=1`)
             .then(function (response) {
                 data = response.data;
 
+                Fajarbox.addEventListener("mouseover",function show() {
+                    Fajar.innerHTML = "Timing:" +"  "+ data.data.timings.Fajr;
+                })
+                Zoharbox.addEventListener("mouseover",function show() {
+                    Zohar.innerHTML = "Timing:" +"  "+ data.data.timings.Dhuhr;
+                })
+                Asarbox.addEventListener("mouseover",function show() {
+                    Asar.innerHTML = "Timing:" +"  "+ data.data.timings.Asr;
+                })
+                Maghribbox.addEventListener("mouseover",function show() {
+                    Maghrib.innerHTML = "Timing:" +"  "+ data.data.timings.Maghrib;
+                })
+                Eshabox.addEventListener("mouseover",function show() {
+                    Esha.innerHTML = "Timing:" +"  "+ data.data.timings.Isha;
+                })
                 
-                document.getElementById('Fajar').innerHTML = "Timing:" +"  "+ data.data.timings.Fajr;
-                document.getElementById('Zohar').innerHTML = "Timing:" +"  "+ data.data.timings.Dhuhr;
-                document.getElementById('Asar').innerHTML = "Timing:" +"  "+ data.data.timings.Asr;
-                document.getElementById('Maghrib').innerHTML = "Timing:" +"  "+ data.data.timings.Maghrib;
-                document.getElementById('Esha').innerHTML = "Timing:" +"  "+ data.data.timings.Isha;
+                Fajarbox.addEventListener("mouseout",function show() {
+                    Fajar.innerHTML = "Hover to see";
+                })
+                Zoharbox.addEventListener("mouseout",function show() {
+                    Zohar.innerHTML = "Hover to see";
+                })
+                Asarbox.addEventListener("mouseout",function show() {
+                    Asar.innerHTML = "Hover to see";
+                })
+                Maghribbox.addEventListener("mouseout",function show() {
+                    Maghrib.innerHTML = "Hover to see";
+                })
+                Eshabox.addEventListener("mouseout",function show() {
+                    Esha.innerHTML = "Hover to see";
+                })
+                
+                
                 main.style.display = 'flex';
                 form.style.display = 'none';
                 
             })
 
         }
+let change = ()=>{
+    form.style.display = 'flex';
+    main.style.display = 'none';
+}
